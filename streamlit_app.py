1e7e8a8eea84ef9a768b40508b1566c4e91f6b09
pguptadev <pavan.c.kumar.gupta@accenture.com>
2026-04-20 19:59:37 +0530

    Update streamlit_app.py
    To error out

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,42 +3,43 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 #App Code
-st.title(fcup_with_straw Customize your Smoothiecup_with_straw)
+st.title(f":cup_with_straw: Customize your Smoothie:cup_with_straw:")
 st.write(
-    Choose the fruit you want in your custom smoothie.
-    )
+    """Choose the fruit you want in your custom smoothie.
+    """)
 
-Order_name = st.text_input('Name on Smoothie')
-st.write('Name on the smoothie will be',Order_name)
+Order_name = st.text_input('Name on Smoothie:')
+st.write('Name on the smoothie will be:',Order_name)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-My_df = session.table(SMOOTHIES.PUBLIC.FRUIT_OPTIONS).select(col('FRUIT_NAME'))
+
+My_df = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
 #st.dataframe(data = My_df, use_container_width=True)
 
 ingredients_list = st.multiselect(
-    'Choose upto 5 engrediants'
+    'Choose upto 5 engrediants:'
     ,My_df
     ,max_selections=5)
 
-if ingredients_list
+if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
 
     ingredients_string = ''
 
-    for each_fruit in ingredients_list
+    for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
             
     #st.write(ingredients_string)
 
-    insert_stmt = insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS,NAME_ON_ORDER) 
-    values(' + ingredients_string + ', ' + Order_name + ')
+    insert_stmt = """insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS,NAME_ON_ORDER) 
+    values('""" + ingredients_string + """', '""" + Order_name + """')"""
 
     #st.write(insert_stmt)
         
     time_to_insert = st.button('Submit Order')
 
-    if time_to_insert
+    if time_to_insert:
         session.sql(insert_stmt).collect()
-        st.success(fYour Smoothie is ordered, {Order_name} !, icon=✅)
+        st.success(f"Your Smoothie is ordered, {Order_name} !", icon="✅")
